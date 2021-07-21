22fe67da1decf206efd3a6a521d6cd4d19f33309
GLYASAI <chaoschunqfeijg@163.com>
2021-07-21 17:52:07 +0800

    ensure ClusterRoleBinding rainbond-operator

--- internal/task/init_rainbond_cluster.go
+++ internal/task/init_rainbond_cluster.go
@@ -34,16 +34,17 @@ import (
 	"github.com/sirupsen/logrus"
 	apiv1 "goodrain.com/cloud-adaptor/api/cloud-adaptor/v1"
 	"goodrain.com/cloud-adaptor/internal/adaptor/factory"
-	"goodrain.com/cloud-adaptor/internal/usecase"
 	"goodrain.com/cloud-adaptor/internal/datastore"
 	"goodrain.com/cloud-adaptor/internal/operator"
 	"goodrain.com/cloud-adaptor/internal/repo"
 	"goodrain.com/cloud-adaptor/internal/types"
+	"goodrain.com/cloud-adaptor/internal/usecase"
 	"goodrain.com/cloud-adaptor/pkg/util/constants"
 	"goodrain.com/cloud-adaptor/version"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 //InitRainbondCluster init rainbond cluster
@@ -139,6 +140,13 @@ func (c *InitRainbondCluster) Run(ctx context.Context) {
 		c.rollback("InitRainbondRegionOperator", "can not select eip", "failure")
 		return
 	}
+
+	// make sure ClusterRoleBinding rainbond-operator not exists.
+	if err := c.ensureClusterRoleBinding(ctx, coreClient); err != nil {
+		c.rollback("InitRainbondRegionOperator", err.Error(), "failure")
+		return
+	}
+
 	rri := operator.NewRainbondRegionInit(*kubeConfig, repo.NewRainbondClusterConfigRepo(datastore.GetGDB()))
 	if err := rri.InitRainbondRegion(initConfig); err != nil {
 		c.rollback("InitRainbondRegionOperator", err.Error(), "failure")
@@ -161,7 +169,7 @@ func (c *InitRainbondCluster) Run(ctx context.Context) {
 		}
 		status, err := rri.GetRainbondRegionStatus(initConfig.ClusterID)
 		if err != nil {
-			if errors.IsNotFound(err) {
+			if k8sErrors.IsNotFound(err) {
 				c.rollback("InitRainbondRegion", err.Error(), "failure")
 				return
 			}
@@ -258,6 +266,27 @@ func (c *InitRainbondCluster) Stop() error {
 //GetChan get message chan
 func (c *InitRainbondCluster) GetChan() chan apiv1.Message {
 	return c.result
+}
+
+func (c *InitRainbondCluster) ensureClusterRoleBinding(ctx context.Context, kubeClient kubernetes.Interface) error {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	crb, err := kubeClient.RbacV1().ClusterRoleBindings().Get(ctx, "rainbond-operator", metav1.GetOptions{})
+	if err != nil {
+		if k8sErrors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+
+	if err := kubeClient.RbacV1().ClusterRoleBindings().Delete(ctx, crb.Name, metav1.DeleteOptions{}); err != nil {
+		if k8sErrors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
 
 func getK8sNode(node v1.Node) *rainbondv1alpha1.K8sNode {
